3ecc6f7babd86893c4ba7d0d98b0accb3f7a7fc8
federico <73360688+fedevelez0@users.noreply.github.com>
2024-10-03 07:02:06 -0500

    App.py

--- App.py
+++ App.py
@@ -1,30 +1,15 @@
-import tensorflow as tf
-from PIL import Image, ImageOps
-import numpy as np
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-# Function to predict digit
-def predictDigit(image):
-    model = tf.keras.models.load_model("model/handwritten.h5")
-    image = ImageOps.grayscale(image)
-    img = image.resize((28, 28))
-    img = np.array(img, dtype='float32')
-    img = img / 255
-    img = img.reshape((1, 28, 28, 1))
-    pred = model.predict(img)
-    result = np.argmax(pred[0])
-    return result
-
 # Streamlit layout
-st.set_page_config(page_title='Tablero para dibujo', layout='wide')
+st.set_page_config(page_title='Tablero para Dibujo', layout='wide')
 st.title('✨ Tablero para Dibujo ✨')
 
 # Sidebar settings
 st.sidebar.title("🎨 Propiedades del Tablero")
 drawing_mode = st.sidebar.selectbox("Herramienta de Dibujo", ["freedraw"])
 stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 15)
-stroke_color = '#FF6347'  # Cambiado a un color más vibrante (Tomato)
+stroke_color = '#FF6347'  # Color vibrante (Tomato)
 bg_color = '#1E1E1E'       # Fondo oscuro
 
 # Create canvas

8bd671a5ca67c0d53cf4e4ea0c3c781db5021f27
federico <73360688+fedevelez0@users.noreply.github.com>
2024-10-03 06:57:11 -0500

    App.py

--- App.py
+++ App.py
@@ -1,5 +1,23 @@
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+# App
+def predictDigit(image):
+    model = tf.keras.models.load_model("model/handwritten.h5")
+    image = ImageOps.grayscale(image)
+    img = image.resize((28,28))
+    img = np.array(img, dtype='float32')
+    img = img/255
+    plt.imshow(img)
+    plt.show()
+    img = img.reshape((1,28,28,1))
+    pred= model.predict(img)
+    result = np.argmax(pred[0])
+    return result
 
 # Streamlit layout
 st.set_page_config(page_title='Tablero para dibujo', layout='wide')
@@ -23,4 +41,12 @@ canvas_result = st_canvas(
     key="canvas",
 )
 
-
+# Add buttons for saving and clearing
+col1, col2 = st.columns(2)
+with col1:
+    if st.button('Guardar'):
+        st.write("Imagen guardada.")
+with col2:
+    if st.button('Borrar'):
+        st.session_state.canvas_data = None
+        st.experimental_rerun()
